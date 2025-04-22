1e2015522fda9a4116cf4dcf5949f47fde8dbab8
Peter Tso <tsopeter@ku.edu>
2025-04-21 22:54:20 -0500

    Changes

--- NAS/train-cel.py
+++ NAS/train-cel.py
@@ -200,8 +200,6 @@ train_loader  = DataLoader(train_dataset, network_params["bsz"], shuffle=True)
 test_loader   = DataLoader(test_dataset, network_params["bsz"], shuffle=False)
 val_loader    = DataLoader(val_dataset, network_params["bsz"], shuffle=False)
 
-model = get_network(3,2,0.5, output_features=3, use_softmax=True, check_inputs=False, quantized=True).to(device)
-
 print(f'Training Data')
 print(f'\tTraining Dataset: {len(train_dataset)}, {get_dataset_distribution(train_dataset.dataset.turns)}')
 print(f'\tTesting Dataset: {len(test_dataset)}, {get_dataset_distribution(test_dataset.dataset.turns)}')
@@ -209,20 +207,15 @@ print(f'\tTesting Dataset: {len(test_dataset)}, {get_dataset_distribution(test_d
 print(f'Running brute force')
 
 class_weights[class_weights<1]=0.5
-#best_model, best_loss, config = brute(stats, train_loader, test_loader, val_loader, weight=class_weights)
-
-
-best_model = get_network(5,3,0.5,output_features=3,use_softmax=True)
-config = [0]
-best_loss = [0]
+best_model, best_loss, config = brute(stats, train_loader, test_loader, val_loader, weight=class_weights)
 
 name   = f'{datetime.now()}'.replace(' ', '-').replace(':', '-').replace('.', '-')
 ready_model_filename = model_dir + f'/{name}.onnx'
 input_a = np.random.randint(0, 255, size=(1,1,network_params["image_height"], network_params["image_width"])).astype(np.float32)
-input_t = torch.from_numpy(input_a).to(device)
+input_t = torch.from_numpy(input_a)
 
 best_model.cpu()
-export_qonnx(model, export_path=ready_model_filename, input_t=input_t)
+export_qonnx(best_model, export_path=ready_model_filename, input_t=input_t)
 qonnx_cleanup(ready_model_filename, out_file=ready_model_filename)
 
 model = ModelWrapper(ready_model_filename)
